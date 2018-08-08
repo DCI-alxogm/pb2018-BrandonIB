916933f94e92e73fefd124de15a96a896a911ff6
BrandonIB <brandonbehc@gmail.com>
2018-08-08 09:39:35 -0500

    Brandon Emmanuel HernándezCorona

---
+++ semana1/programa_hola.c
@@ -0,0 +1,22 @@
+/*Escribe un texto como el siguiente : */
+
+/*
+ Esto es un comentario. Me sirve para escribir información relevante del programa que estoy haciendo. Por  ejemplo:
+ Este programa imprime la típica frase “hola mundo a la pantalla”
+ Creado el 06/08/2018 por … 	Edit by: Brandon Emmanuel Hernández Corona; Ing. Biomédica
+*/
+
+
+#include<stdio.h>
+
+int main()
+{
+printf("Hola Brandon\n");
+printf("Este es el primer programa del curso\n");
+}
+
+/*Terminé de escribir el programa*/
+
+
+
+
