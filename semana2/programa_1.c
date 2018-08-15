9a4a09d4375aedbe72d19339e0a1949915115cd7
BrandonIB <brandonbehc@gmail.com>
2018-08-15 08:22:15 -0500

    BRANDONEMMANUELHERNANDEZCORONA

---
+++ semana2/programa_1.c
@@ -0,0 +1,17 @@
+/*creado por BRANDON EMMANUEL HERNÁNDEZ CORONA*/
+
+#include<stdio.h>
+
+
+int main()
+{
+
+int edad;
+printf("¿Cuál es tu edad?");
+scanf("%i",&edad);
+printf("\nTu edad es: %i \n",edad);
+
+return 0;
+
+}
+
