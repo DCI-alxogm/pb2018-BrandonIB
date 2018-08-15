24e6bdf0d2d1695a7a14e8c89d292e85b3f7a693
BrandonIB <brandonbehc@gmail.com>
2018-08-15 09:46:36 -0500

    BRANDONEMMANUELHERNANDEZCORONA

---
+++ semana2/ejercicio_1.c
@@ -0,0 +1,21 @@
+/*creado por BRANDON EMMANUEL HERNÁNDEZ CORONA*/
+
+#include<stdio.h>
+
+
+int main()
+{
+
+int edad;
+int edad_2;
+
+
+printf("¿Cuál es tu edad?");
+scanf("%i",&edad);
+edad_2 = edad + 10;
+printf("\nTu edad en el 2018 es: %i \n",edad_2);
+
+return 0;
+
+}
+
