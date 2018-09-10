7fca8121478bdcf22836d0edb10acdf53d6d9d76
BrandonIB <42146715+BrandonIB@users.noreply.github.com>
2018-09-10 09:55:56 -0500

    BRANDON EMMANUEL HERNANDEZ CORONA

---
+++ semana6/semana6_ejemplos123.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Compilación clase
+int main(){
+int j,n;
+printf("dime el numero de elementos para trabajar\n");
+scanf("%i",&n);
+float numeros[n];
+for(j=0;j<n;j++){
+    scanf("%f",&numeros[j]);
+    numeros[j]*=2;
+    printf("%f\n",numeros[j]);
+    }
+    return 0;
+    }
+
+/*
+//EJEMPLO 1
+int main()
+{
+int i, N=10;
+float numeros[N];
+for(i=0;i<N;i++){
+    numeros[i]=0;
+}
+    return 0;
+}
+//EJEMPLO 2
+#include <stdlib.h>
+#include <stdio.h>
+int main(){
+for(i=0;i<N;i++){
+scanf("%f", &numeros[i]);//asigna el valor a partir de lo que proporciona el usuario, línea por línea.
+}
+return 0;
+}
+//EJEMPLO 3
+#include <stdlib.h>
+#include <stdio.h>
+int main(){
+for(i=0;i<N;i++){
+printf("%f\n",numeros[i]);//imprime a la pantalla el valor del elemento i-ésimo del arreglo.
+}
+return 0;
+}
+*/
