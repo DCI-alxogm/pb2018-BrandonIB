740e08429bfe6fbdad7c4b010798a80f4921d226
BrandonIB <42146715+BrandonIB@users.noreply.github.com>
2018-10-11 00:09:43 -0500

    BRANDON EMMANUEL HERNANDEZ CORONA

---
+++ semana9/semana9_ejercicio1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE* fp=NULL;
+    char* nombrearchivo="num_maximo.txt";
+
+float a[95];
+float *a1;
+    //a1=&a[90];
+a1=malloc(95*sizeof(float));
+int i=0;
+float numayor;
+
+fp = fopen(nombrearchivo, "r+");
+        if(fp==NULL)return -1;
+
+    for (i=0;i<95;i++){
+        fscanf(fp,"%f", &a[i]);
+        a1=&a[i];
+        //printf("%f\n", a[i]);
+        //printf("%f\n", *a1);
+        //printf("%p\n", *a1);
+        }
+
+        numayor=a[0];
+    for (i=0;i<95;i++){
+        if (a[i]>numayor){
+            numayor=a[i];
+        }
+    }
+
+printf("El numero mayor  es %f\n", numayor);
+
+
+    return 0;
+}
---
+++ semana9/semana9_ejercicio2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+int main()
+{
+
+
+FILE* fp=NULL;
+char* nombrearchivo="texto2.txt";
+
+int numeros[5];
+int *p_numeros;
+    p_numeros = malloc(5*sizeof(int));
+int i;
+float sum, prom, suma0, suma1, suma2, suma3, suma4, cociente, S;
+
+fp = fopen(nombrearchivo, "r+");
+if(fp==NULL)return -1;
+
+for (i=0;i<5;i++){
+    fscanf(fp,"%d",&numeros[i]);
+    p_numeros=&numeros[i];
+    //printf("%d\n", numeros[i]);
+    //printf("%d\n\n", *p_numeros);
+    printf("La locacion del primer a ultimo numero en la memoria dinamica es: %p\n", *p_numeros);
+}
+    sum = numeros[0]+numeros[1]+numeros[2]+numeros[3]+numeros[4];
+    prom = (sum)/5;
+    //printf("%f\n", prom);
+    suma0=pow((numeros[0])-prom,2);
+    suma1=pow((numeros[1])-prom,2);
+    suma2=pow((numeros[2])-prom,2);
+    suma3=pow((numeros[3])-prom,2);
+    suma4=pow((numeros[4])-prom,2);
+    cociente=(suma0+suma1+suma2+suma3+suma4)/4;
+    S=sqrt(cociente);
+    printf("La Desviacion Estandar S de los datos del texto es: %f\n", S);
+
+    free(p_numeros);
+
+
+    return 0;
+}
